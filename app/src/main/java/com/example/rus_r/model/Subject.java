c04fb4d334482b2a1fe9e499758fb52930e56bdf
Mark Gerald Pacun <markgpacun@gmail.com>
2026-05-19 21:54:33 +0800

    Subject.java edited

--- app/src/main/java/com/example/rus_r/model/Subject.java
+++ app/src/main/java/com/example/rus_r/model/Subject.java
@@ -1,4 +1,15 @@
 package com.example.rus_r.model;
 
 public class Subject {
+    private String id;
+    private String name;
+    private String description;
+    private String color;
+    private long createdAt;
+    private long updatedAt;
+    private int lessonCount;
+    private int quizCount;
+    private int flashcardCount;
+
+
 }
